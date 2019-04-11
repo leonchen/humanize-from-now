7d3583257205c874c678b8691826277f8c506912
Leon Chen <leonhart.chen@gmail.com>
2019-04-11 23:36:05 +0800

    v0.0.2: fix time ranges

--- index.ts
+++ index.ts
@@ -1,43 +1,43 @@
 // moment fromNow() like humanized duration, see
 // http://momentjs.com/docs/#/displaying/fromnow/
 
-const MS = 1 / 1000;
-const SECOND = 1;
-const MINUTE = 60;
-const HOUR = MINUTE * 60;
-const DAY = HOUR * 24;
-const MONTH = DAY * 30.5;
-const YEAR = MONTH * 12;
+const MS = 1;
+const SECOND = 1000 * MS;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+const MONTH = 30.5 * DAY;
+const YEAR = 12 * MONTH;
 
-const AS_JUST_NOW = 3 * SECOND;
-const AS_SECONDS = 44 * SECOND;
-const AS_A_MINUTE = 89 * SECOND;
-const AS_MINUTES = 44 * MINUTE;
-const AS_AN_HOUR = 89 * MINUTE;
-const AS_HOURS = 21 * HOUR;
-const AS_A_DAY = 35 * HOUR;
-const AS_DAYS = 25 * DAY;
+const AS_JUST_NOW = 5 * SECOND;
+const AS_SECONDS = 45 * SECOND;
+const AS_A_MINUTE = 90 * SECOND;
+const AS_MINUTES = 45 * MINUTE;
+const AS_AN_HOUR = 90 * MINUTE;
+const AS_HOURS = 22 * HOUR;
+const AS_A_DAY = 36 * HOUR;
+const AS_DAYS = 26 * DAY;
 const AS_A_MONTH = 45 * DAY;
-const AS_MONTHS = 319 * DAY;
-const AS_A_YEAR = 547 * DAY;
+const AS_MONTHS = 320 * DAY;
+const AS_A_YEAR = 548 * DAY;
 
 export default function(time: number | string | Date, failover: string = 'N/A'): string {
   const t = typeof time !== 'number' ? new Date(time).getTime() : time;
   if (isNaN(t)) return failover;
   const now = Date.now();
-  const diff = (now - t) * MS;
+  const diff = now - t;
 
   if (diff < 0) return failover;
   if (diff < AS_JUST_NOW) return 'just now';
   if (diff < AS_SECONDS) return 'a few seconds ago';
   if (diff < AS_A_MINUTE) return 'a minute ago';
-  if (diff < AS_MINUTES) return `${Math.ceil(diff / MINUTE)} minutes ago`;
+  if (diff < AS_MINUTES) return `${Math.round(diff / MINUTE)} minutes ago`;
   if (diff < AS_AN_HOUR) return 'an hour ago';
-  if (diff < AS_HOURS) return `${Math.ceil(diff / HOUR)} hours ago`;
+  if (diff < AS_HOURS) return `${Math.round(diff / HOUR)} hours ago`;
   if (diff < AS_A_DAY) return 'a day ago';
-  if (diff < AS_DAYS) return `${Math.ceil(diff / DAY)} days ago`;
+  if (diff < AS_DAYS) return `${Math.round(diff / DAY)} days ago`;
   if (diff < AS_A_MONTH) return 'a month ago';
-  if (diff < AS_MONTHS) return `${Math.ceil(diff / MONTH)} months ago`;
+  if (diff < AS_MONTHS) return `${Math.round(diff / MONTH)} months ago`;
   if (diff < AS_A_YEAR) return 'a year ago';
-  return `${Math.ceil(diff / YEAR)} years ago`;
+  return `${Math.round(diff / YEAR)} years ago`;
 }
